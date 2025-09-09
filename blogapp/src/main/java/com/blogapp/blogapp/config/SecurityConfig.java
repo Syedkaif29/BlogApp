bf00f0c5cc85b31ce79cf4c040f07ac82660b89d
unknown <syedkaif5955@gmail.com>
2025-09-10 02:27:13 +0530

    Fix missing imports in SecurityConfig

--- blogapp/src/main/java/com/blogapp/blogapp/config/SecurityConfig.java
+++ blogapp/src/main/java/com/blogapp/blogapp/config/SecurityConfig.java
@@ -1,8 +1,10 @@
 package com.blogapp.blogapp.config;
 
 import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
